06f86b7751d3668c063eaf214b8754cc8ba46110
Jacob McCarthy <thejacobmccarthy@gmail.com>
2021-11-12 10:46:13 -0500

    set up intial file structure, installed require packages, begining work on inquirer prompts

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
+const jest = require('jest');
+
+const table = require('console.table');
+
+
+
+
+function mainMenu() {
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "What would you like to do?",
+            name: "next",
+            choices: [
+                'View all departments',
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update and employee role'
+            ]
+        }])
+        .then((answers) => console.log(answers.next))
+        
+}
+
+function viewDepts(){
+
+}
+
+mainMenu()
